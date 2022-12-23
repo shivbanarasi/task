8b125a4e466daaca137b179000c888328a38aebe
shivbanarasi <shivbanarasi0542@gmail.com>
2022-12-23 09:08:26 +0530

    changed color green

--- dom.js
+++ dom.js
@@ -6,7 +6,11 @@ console.log(document.head);
 console.log(document.doctype);
  document.all[10].textContent="hello";
  console.log(document.links);
- let headerTitle=document.getElementById('header_title');
+ let headerTitle=document.getElementById("header_title");
  console.log(headerTitle);
- headerTitle.textContent='goodbye';
- headerTitle.innerText='goodbye';
+ headerTitle.textContent="goodbye";
+ //headerTitle.innerText="goodbye";
+ headerTitle.style.borderBottom='solid 3px #000';
+ //document.all[10].style.borderBottom="solid 3px #000"
+ document.getElementById("header_title").textContent="hello";
+ document.getElementsByClassName("title").style.color="green";
