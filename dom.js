4a7ae068e48343bf68b3ad7455599612159a6d74
shivbanarasi <shivbanarasi0542@gmail.com>
2022-12-23 07:58:08 +0530

    dom js learning

---
+++ dom.js
@@ -0,0 +1,12 @@
+//console.dir(document);
+console.log(document.URL);
+console.log(document.domain);
+console.log(document.title);
+console.log(document.head);
+console.log(document.doctype);
+ document.all[10].textContent="hello";
+ console.log(document.links);
+ let headerTitle=document.getElementById('header_title');
+ console.log(headerTitle);
+ headerTitle.textContent='goodbye';
+ headerTitle.innerText='goodbye';
